4d891f608ed8b131fa4e800e31bf8ea7fefe8192
Soumen <soumen.trivedi@arkayaventure.co.uk>
2014-10-21 10:35:52 +0100

    Updated code to remove duplicate repository definition. Using repo definition from platform_repos instead.

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,5 +1,5 @@
 class beaker_platform::setup {
-  require beaker_platform::repo
+  require platform_repos
   require beaker_platform::setup_beaker
   require beaker_platform::setup_docker
   require beaker_platform::setup_gitreview
